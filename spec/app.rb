bd96eff685924824d756afa7ee9d96b52b5b38b0
Kenneth Jordan <kensworldc@gmail.com>
2022-03-13 16:17:26 -0400

    review from mac

--- spec/app.rb
+++ spec/app.rb
@@ -1,23 +1,31 @@
 
 
-# is the new // 
-# puts does a console log 
-# puts('Hello world!')
+# # is the new // 
+# # puts does a console log 
+# # puts('Hello world!')
 
-puts "Hello world!"
-puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello World!"
+# puts [1, 2, 3]
+# p [1, 2, 3] # displays console log in a horizontal line rather than vertical 
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# print [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# # pp - pretty-printing for complex data such as nested arrays and hashes  
+
+
+# print "Pass this test, please."
+
+# changes from Mac 
+
 puts "Hello World!"
-puts [1, 2, 3]
-p [1, 2, 3] # displays console log in a horizontal line rather than vertical 
+print " Pass this test, please."
+p [1, 2, 3,]
+ 
 
-print "Hello world!"
-print "Hello world!"
-print "Hello world!"
-print [1, 2, 3]
-
-pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
-# pp - pretty-printing for complex data such as nested arrays and hashes  
-
-
-print "Pass this test, please."
 
